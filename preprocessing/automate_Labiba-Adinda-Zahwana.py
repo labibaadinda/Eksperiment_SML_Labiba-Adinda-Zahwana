2cb7ece0895737134558615f80841949a4bdc021
labibaadinda <labibaadinda11@gmail.com>
2025-06-22 19:53:13 +0700

    change requirements

--- preprocessing/automate_Labiba-Adinda-Zahwana.py
+++ preprocessing/automate_Labiba-Adinda-Zahwana.py
@@ -2,8 +2,6 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 def preprocess_sleep_data(df):
     """
